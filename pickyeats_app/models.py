4f6f11cd4ef5bed63b0e72b1c306ed7883199611
kadrianne <kristine.a.du@gmail.com>
2020-05-15 19:21:35 -0600

    Create models,views,serializers.

--- pickyeats_app/models.py
+++ pickyeats_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Party(models.Model):
+    title = models.CharField(max_length=30)
+    active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class User(models.Model):
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    active_party = models.ForeignKey(Party, blank=True, null=True, on_delete=models.SET_NULL)
+
+class LikedRestaurant(models.Model):
+    name = models.CharField(max_length=50)
+    yelp_id = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
---
+++ pickyeats_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('users', views.UserView)
+router.register('liked-restaurants', views.LikedRestaurantView)
+router.register('party', views.PartyView)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
