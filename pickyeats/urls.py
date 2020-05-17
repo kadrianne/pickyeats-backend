99362afac1638365cb4250db8ba2d8ac6d0da594
kadrianne <kristine.a.du@gmail.com>
2020-05-17 09:19:35 -0600

    Add Silk debugging tool.

--- pickyeats/urls.py
+++ pickyeats/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pickyeats_app.urls'))
 ]
+urlpatterns += [url(r'^silk/', include('silk.urls', namespace='silk'))]
